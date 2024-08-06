b9dea12e7950f7148a2a821f9e06c36b8562cec7
Daniel <dcolinmorgan@gmail.com>
2024-08-06 13:40:48 +0200

    init site

---
+++ docs/components/header.js
@@ -0,0 +1,56 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    nav {
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color:  #0a0a23;
+    }
+
+    ul {
+      padding: 0;
+    }
+
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+
+    a {
+      font-weight: 700;
+      margin: 0 25px;
+      color: #fff;
+      text-decoration: none;
+    }
+
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #fff;
+    }
+  </style>
+  <header>
+        <h1>Modegen.ai</h1>
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="contact.html">Contact</a>
+            <a href="team.html">Team</a>
+        </nav>
+  </header>
+`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(headerTemplate.content);
+  }
+}
+
+customElements.define('header-component', Header);
